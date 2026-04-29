412bda33eda01b3cda65337d6ca732e98a366ec9
Jonas Knobloch <jonas.knobloch@t-online.de>
2026-04-29 14:37:30 +0200

    WIP

--- scripts/run_gpt2_774M.sh
+++ scripts/run_gpt2_774M.sh
@@ -5,7 +5,7 @@
 # on 8X A100 80GB SXM ($14/hr) steps in ~1.7s/iter
 # => training time 286,102 steps * 1.7s = 135 hours ~= 5.6 days ~= $2000 (10X 124M)
 
-make train_gpt2cu USE_CUDNN=1
+make train_gpt2cu USE_CUDNN=0 # SDPA FP16/BF16 forward is not supported on cuDNN 9.10.0/9.10.1 due to known bugs. Please consider upgrading to 9.10.2 or newer.
 out_dir="log_gpt2_774M"
 done_file="$out_dir/DONE_00286102"
 
@@ -20,13 +20,13 @@ while true; do
 
     # run python dev/data/fineweb.py --version 100B to prepro data
     # run python dev/data/hellaswag.py to prepro hellaswag eval
-    mpirun -np 8 ./train_gpt2cu \
-                -i "dev/data/fineweb100B/fineweb_train_*.bin" \
-                -j "dev/data/fineweb100B/fineweb_val_*.bin" \
+    mpirun -np 1 --bind-to none ./train_gpt2cu \
+                -i "dev/data/minipile/m000/minipile_train_*.bin" \
+                -j "dev/data/minipile/m000/minipile_val_*.bin" \
                 -o $out_dir \
-                -v 250 -s 300000 -g 144 \
-                -h 1 \
-                -b 32 -t 1024 \
+                -v 250 -s 500 -g 144 \
+                -h 0 \
+                -b 16 -t 1024 \
                 -d 524288 \
                 -r 0 \
                 -z 1 \
@@ -35,7 +35,7 @@ while true; do
                 -q 0.0 \
                 -u 700 \
                 -n 4000 \
-                -x 286102 \
+                -x 2810 \
                 -y 1 \
                 -e "d36"
 
